8dacea0d6792f6b1fd6d8b18c64a1a7f416ad206
Giacomo <giacomo.licari@gmail.com>
2018-03-09 16:35:17 +0100

    Added main entry point, added support for deploy of multiple markets

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { readFile } from './utils/os'
+import { readFile, fileExists } from './utils/os'
+import { DEFAULT_CONFIG_FILE_PATH, DEFAULT_MARKET_FILE_PATH } from './utils/constants'
 import FileWriter from './utils/fileWriter'
 import ConfigValidator from './validators/configValidator'
 import MarketValidator from './validators/marketValidator'
@@ -6,51 +7,64 @@ import CentralizedOracle from './oracles/centralizedOracle'
 import CategoricalEvent from './events/categoricalEvent'
 import ScalarEvent from './events/scalarEvent'
 import Market from './markets'
+import Token from './tokens'
+import minimist from 'minimist'
 
-const createOracle = (eventDescription, configInstance) => {
-  const oracle = new CentralizedOracle(eventDescription, configInstance)
-  oracle.create()
-  return oracle
+const getMarketStep = marketDescription => {
+  const steps = ['oracleAddress', 'eventAddress', 'marketAddress']
+  let step = -1
+  for (let x in steps) {
+    if (!(steps[x] in marketDescription)) {
+      return step
+    } else if (steps[x] in marketDescription && (
+      marketDescription[steps[x]] === null ||
+      marketDescription[steps[x]] === undefined ||
+      marketDescription[steps[x]].trim() === '')) {
+      return step
+    }
+    step = x
+  }
+  return step
 }
 
-const createEvent = (eventDescription, configInstance) => {
+const createOracle = async (eventDescription, configInstance) => {
+  const oracle = new CentralizedOracle(eventDescription, configInstance)
+  await oracle.create()
+  eventDescription.oracleAddress = oracle.getAddress()
+  return eventDescription
+}
+
+const createEvent = async (eventDescription, configInstance) => {
   let event
   if (eventDescription.outcomeType === 'SCALAR') {
     event = new ScalarEvent(eventDescription, configInstance)
   } else {
     event = new CategoricalEvent(eventDescription, configInstance)
   }
-  event.create()
-  return event
+  await event.create()
+  eventDescription.eventAddress = event.getAddress()
+  return eventDescription
 }
 
-const createMarket = (marketDescription, configInstance) => {
+const createMarket = async (marketDescription, configInstance) => {
   const market = new Market(marketDescription, configInstance)
-  market.create()
-  return market
+  await market.create()
+  marketDescription.marketAddress = market.getAddress()
+  return marketDescription
 }
 
 const fundMarket = () => {
   // TODO
 }
 
-const checkMarketFileAlreadyExists = marketFilePath => {
-  //
-}
-
-const runProcessStack = (configPath, marketReadPath, marketWritePath) => {
+const runProcessStack = async (configPath, marketDescription, step) => {
   // TODO fund + tests
-  // TODO recovery process, start executing from a specific point
-  let configInstance, oracle, event, market, fund
-  // Instantiate file writer
-  const fileWriter = new FileWriter(marketWritePath)
-  // Read market description file
-  const marketDescription = readFile(marketReadPath)
+  let configInstance
   // Validate user file configuration
   const configValidator = new ConfigValidator(configPath)
   try {
-    configValidator.isValid()
-    configValidator.normalize()
+    await configValidator.isValid()
+    await configValidator.normalize()
     configInstance = configValidator.getConfig()
   } catch (error) {
     console.warn(error)
@@ -66,39 +80,101 @@ const runProcessStack = (configPath, marketReadPath, marketWritePath) => {
     process.exit(1)
   }
 
-  // TODO check if the marketWritePath already exists
-  // and ask the user whether use it or override it
+  const steps = {
+    '-1': [createOracle, createEvent, createMarket],
+    '0': [createEvent, createMarket],
+    '1': [createMarket],
+    '2': []
+  }
 
-  // Create oracle
-  oracle = createOracle(marketDescription, configInstance)
-  marketDescription.oracleAddress = oracle.getAddress()
-  fileWriter.setData({
-    'CentralizedOracle': marketDescription.oracleAddress
-  })
-  fileWriter.write()
-  // Create event
-  event = createEvent(marketDescription, configInstance)
-  marketDescription.eventAddress = event.getAddress()
-  fileWriter.setData({
-    'Event': marketDescription.eventAddress
-  })
-  fileWriter.write()
-  // Create market
-  market = createMarket(marketDescription, configInstance)
-  fileWriter.setData({
-    'Market': market.getAddress()
-  })
-  fileWriter.write()
+  // TODO verify has enough funds
+
+  for (let x in steps[step]) {
+    //
+    try {
+      marketDescription = await steps[step][x](marketDescription, configInstance)
+      return marketDescription
+    } catch (error) {
+      console.error(`Got an execption on step ${step}`)
+      console.error(error.message)
+      throw error
+    }
+  }
+
   // Fund market
-  fund = fundMarket()
+  fundMarket()
 }
 
-const main = () => {
-  console.log("### main ###")
-  // TODO
-  // check input params:
-  // -c : configuration file path, default /conf/config.json
-  // -m : market description file path, default /conf/market.json
+/**
+*  Input params:
+* -c : configuration file path, default /conf/config.json
+* -m : market description file path, default /conf/market.json
+*/
+const main = async () => {
+  let configPath, marketPath, marketFile, step
+  configPath = DEFAULT_CONFIG_FILE_PATH
+  marketPath = DEFAULT_MARKET_FILE_PATH
+  // Instantiate file writer
+  const fileWriter = new FileWriter(marketPath, [], false)
+
+  if (process.argv.length === 2) {
+    console.info('Running SDK Utils with default parameters')
+  } else {
+    const args = minimist(process.argv)
+    if (args.f && typeof args.f === 'string') {
+      console.info('Using configuration file: ', args.f)
+      configPath = args.f
+    } else {
+      console.info(`Invalid -f parameter, using default configuration file ${DEFAULT_CONFIG_FILE_PATH}`)
+    }
+    if (args.m && typeof args.m === 'string') {
+      console.info('Using market file: ', args.m)
+      marketPath = args.m
+    } else {
+      console.info(`Invalid -m parameter, using default market file ${DEFAULT_MARKET_FILE_PATH}`)
+    }
+  }
+
+  if (!fileExists(configPath)) {
+    console.warn(`Config file file doesn't exist on path: ${marketPath}`)
+    process.exit(1)
+  }
+  // If the provided (or default) market file doesn't exist,
+  // raise an error and abort
+  if (fileExists(marketPath)) {
+    // read market file JSON content
+    marketFile = readFile(marketPath)
+  } else {
+    console.warn(`Market file doesn't exist on path: ${marketPath}`)
+    process.exit(1)
+  }
+  // Get current market step from market file
+  let marketFileCopy = marketFile.slice()
+  let abort = false
+
+  console.info('Starting deploy...')
+  try {
+    for (let x in marketFileCopy) {
+      let currentMarket = marketFileCopy[x]
+      step = getMarketStep(currentMarket)
+      console.log('## Step: ', step)
+      let updatedMarket = await runProcessStack(configPath, currentMarket, step)
+      marketFileCopy[x] = Object.assign(currentMarket, updatedMarket)
+    }
+    console.info(`Deploy done, writing updates to ${marketPath}`)
+  } catch (error) {
+    console.error('Writing updates before aborting...')
+    abort = true
+  } finally {
+    fileWriter.setFilePath(marketPath)
+    fileWriter.setData(marketFileCopy)
+    fileWriter.write()
+    if (abort) {
+      process.exit(1)
+    } else {
+      console.info(`Updates written successfully`)
+    }
+  }
 }
 
 main()
