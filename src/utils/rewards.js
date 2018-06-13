8d6407f50846a0b8671d995eb63999542cabefc9
Giacomo Licari <giacomo.licari@gmail.com>
2018-06-13 11:31:38 +0200

    Add claim reward feature

--- src/utils/rewards.js
+++ src/utils/rewards.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import contract from 'truffle-contract'
+import Client from './../clients/ethereum'
+import truffleContract from 'truffle-contract'
 
-
-const tokenABI = [ { "constant": false, "inputs": [ { "name": "spender", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "from", "type": "address" }, { "name": "to", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "owner", "type": "address" }, { "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" } ]
 
 const claimRewards = async configInstance => {
     try {
@@ -9,7 +10,16 @@ const claimRewards = async configInstance => {
             throw new Error('rewardClaimHandler is required')
         }
 
-        const rewardInstance = await configInstance.gnosisJS.contracts.RewardClaimHandler.at(configInstance.rewardClaimHandler.address)
+        // Use another web3
+        const providerUrl = `${configInstance.rewardClaimHandler.blockchain.protocol}://${configInstance.rewardClaimHandler.blockchain.host}:${configInstance.rewardClaimHandler.blockchain.port}`
+        const client = new Client(configInstance.mnemonic, providerUrl)
+        const web3 = client.getWeb3()
+
+        const rewardClaimHandlerArtifact = require('@gnosis.pm/pm-apollo-contracts/build/contracts/RewardClaimHandler.json')
+        const rewardContract = truffleContract(rewardClaimHandlerArtifact)
+        rewardContract.setProvider(client.getProvider())
+        const rewardInstance = rewardContract.at(configInstance.rewardClaimHandler.address)
+
         // Get token from the reward contract instance
         const tokenAddress = await rewardInstance.rewardToken()
 
@@ -31,17 +41,22 @@ const claimRewards = async configInstance => {
         const totalAmount = rewardAmounts.reduce((a, b) => a + b)
 
         // Approve reward token
-        const approveTx = await approve(configInstance, tokenAddress, configInstance.rewardClaimHandler.address, totalAmount)
+        const approveTx = await approve(client.getProvider(), configInstance.account, tokenAddress, configInstance.rewardClaimHandler.address, totalAmount)
 
         // function registerRewards(address[] _winners, uint[] _rewardAmounts, uint duration)
-        const registerTx = await rewardInstance.registerRewards(winnersAddresses, rewardAmounts, configInstance.rewardClaimHandler.duration)
+        const registerTx = await rewardInstance.registerRewards(winnersAddresses, rewardAmounts, configInstance.rewardClaimHandler.duration, { from: configInstance.account, gas: 200000 })
 
+        if (registerTx.receipt && registerTx.receipt.status == '0x0' || registerTx.receipt && registerTx.receipt.status == 0) {
+            throw new Error('Reward Claim transaction failed')
+        }
+
+        return registerTx
     } catch (error) {
-        
+        throw error        
     }
 }
 
-const approve = async (configInstance, tokenAddress, rewardClaimAddress, amount) => {
+const approve = async (provider, sender, tokenAddress, rewardClaimAddress, amount) => {
     let result
 
     if (!tokenAddress) {
@@ -56,12 +71,24 @@ const approve = async (configInstance, tokenAddress, rewardClaimAddress, amount)
       throw new Error(`Cannot approv invalid amounts of Ether, got ${amount} ETH`)
     }
 
-    // Use another web3
-    const tokenContract = await configInstance.gnosisJS.web.eth.contract(tokenABI)
-    const tokenInstance = await tokenContract.at(tokenAddress)
-    result = await tokenInstance.approve(rewardClaimAddress, amount)
+    try {
+        const tokenArtifact = require('@gnosis.pm/pm-apollo-contracts/build/contracts/Token.json')
+        const tokenContract = truffleContract(tokenArtifact)
+        tokenContract.setProvider(provider)
+        const tokenInstance = tokenContract.at(tokenAddress)
 
-    return result
+        // Do tokens transfer approval
+        result = await tokenInstance.approve(rewardClaimAddress, amount, { from: sender })
+
+        if (result.receipt && result.receipt.status == '0x0' || result.receipt && result.receipt.status == 0) {
+            throw new Error('Approve token transaction failed')
+        }
+
+        return result
+
+    } catch (err) {
+        throw err
+    }
   }
 
 module.exports = {
