9bda2a809667f38b9ebace2540b69ff479f00ffc
Giacomo Licari <giacomo.licari@gmail.com>
2018-11-20 09:51:34 +0100

    Fix Wrap Ether not properly handled

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -1,4 +1,8 @@
+import { TX_LOOKUP_TIME } from '../utils/constants'
+import { logInfo, logSuccess } from '../utils/log'
 import { isPlayMoneyToken } from '../utils/tokens'
+import { promisify } from '@gnosis.pm/pm-js'
+import sleep from 'sleep'
 
 class Token {
   constructor (configInstance) {
@@ -12,13 +16,33 @@ class Token {
       throw new Error(`Cannot deposit invalid amounts of Ether, got ${amount} ETH`)
     }
 
+    const gasPrice = this._configInstance.gasPrice
+    const web3 = this._configInstance.blockchainProvider.getWeb3()
+    let txReceipt
+
     if (await isPlayMoneyToken(this._configInstance) == true) {
       const playTokenContract = this._configInstance.gnosisJS.contracts.OlympiaToken.at(this._configInstance.collateralToken)
-      result = await playTokenContract.issue([this._configInstance.account], amount)
+      result = await playTokenContract.issue([this._configInstance.account], amount, { gasPrice })
     } else {
       const etherTokenContract = this._configInstance.gnosisJS.contracts.EtherToken.at(this._configInstance.collateralToken)
-      result = await etherTokenContract.deposit({ value: amount })
+      result = await etherTokenContract.deposit({ value: amount, gasPrice })
     }
+
+    logInfo(`Waiting for Ether Wrapping transaction to be mined, tx hash: ${result.tx}`)
+
+    while (true) {
+      sleep.msleep(TX_LOOKUP_TIME)
+      txReceipt = await promisify(web3.eth.getTransactionReceipt)(result.tx)
+      if (!txReceipt) {
+        continue
+      } else if (txReceipt && parseInt(txReceipt.status) === 0) {
+        // handle error, transaction failed
+        throw new Error('Ether wrapping transaction failed.')
+      } else if (txReceipt && parseInt(txReceipt.status) === 1) {
+        break
+      }
+    }
+
     return result
   }
 
